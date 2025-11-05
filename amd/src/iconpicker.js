449ca67884ef9c277c70a5e6282f7d6c2e662c26
Tobias Garske <tobias.garske@isb.bayern.de>
2025-11-05 14:46:17 +0100

    MBS-10086: fix deprecation of modal factory (#13)

--- amd/src/iconpicker.js
+++ amd/src/iconpicker.js
@@ -1,6 +1,15 @@
 import {exception as displayException} from 'core/notification';
+import {getString} from 'core/str';
 import Templates from 'core/templates';
+import Modal from 'core/modal';
 import ICON_SET from 'block_floatingbutton/iconset';
+
+const SELECTORS = {
+  iconpicker: ".block_floatingbutton-iconpicker",
+  iconpickerInput: ".block_floatingbutton-input input",
+  iconpickerSearchInput: ".block_floatingbutton-iconpicker-search-input",
+  iconpickerIcon: ".block_floatingbutton-iconpicker-icon",
+};
 
 export const init = (iconpickerclass) => {
     // Add index to icon set to make producing an unique id easier.
@@ -9,7 +18,7 @@ export const init = (iconpickerclass) => {
     });
 
     // Make inputs of the moodle form invisible and add button for iconpicker.
-    let inputs = Array.from(document.querySelectorAll('.block_floatingbutton-input input'));
+    let inputs = Array.from(document.querySelectorAll(SELECTORS.iconpickerInput));
     inputs.forEach(function(input) {
         input.setAttribute('style', 'visibility: collapse; width: 0; margin: 0; padding: 0; position: absolute;');
         input.insertAdjacentHTML(
@@ -35,48 +44,51 @@ export const init = (iconpickerclass) => {
  * @param {*} target
  * @param {*} input
  */
-function buildModal(target, input) {
-    // Build iconpicker modal with moodle modal factory
-    Templates.renderForPromise('block_floatingbutton/iconpicker', {target: target, input: input, icons: ICON_SET})
-        .then(({html}) => {
-            require(['jquery', 'core/modal_factory'], function($, ModalFactory) {
-                var trigger = $('#create-modal');
-                ModalFactory.create({
-                    title: 'Icon picker',
-                    body: html,
-                    footer: '',
-                }, trigger)
-                .done(function(modal) {
-                    modal.getRoot().on('modal:shown', function(event) {
-                        let iconpicker = event.target.querySelector('.block_floatingbutton-iconpicker');
-                        // Listeners for the icons and the search input have to be registered when modal is shown for the first
-                        // time because modal doesn't exist in the DOM before.
-                        let search = iconpicker.querySelector('.block_floatingbutton-iconpicker-search-input');
-                        search.addEventListener('input', searchicon);
-                        let icons = Array.from(iconpicker.querySelectorAll('.block_floatingbutton-iconpicker-icon'));
-                        icons.forEach(function(icon) {
-                            icon.addEventListener('click', (e) => {
-                                iconclick(e);
-                                modal.hide();
-                            });
-                        });
-                    });
-                    modal.show();
-                    highlightselected();
+const buildModal = async(target, input) => {
+    try {
+        const {html} = await Templates.renderForPromise('block_floatingbutton/iconpicker', {
+            target: target,
+            input: input,
+            icons: ICON_SET
+        });
+
+        const title = await getString('icon_picker', 'block_floatingbutton');
+        const modal = await Modal.create({
+            title: title,
+            body: html,
+            footer: '',
+        });
+
+        modal.getRoot().on('modal:shown', function(event) {
+            const iconpicker = event.target.querySelector(SELECTORS.iconpicker);
+            // Listeners for the icons and the search input have to be registered when modal is shown for the first
+            // time because modal doesn't exist in the DOM before.
+            const search = iconpicker.querySelector(SELECTORS.iconpickerSearchInput);
+            search.addEventListener('input', searchicon);
+            const icons = Array.from(iconpicker.querySelectorAll(SELECTORS.iconpickerIcon));
+            icons.forEach(function(icon) {
+                icon.addEventListener('click', (e) => {
+                    iconclick(e);
+                    modal.hide();
                 });
             });
-            return true;
-        }).catch(ex => displayException(ex));
-}
+        });
+
+        modal.show();
+        highlightselected();
+    } catch (ex) {
+        displayException(ex);
+    }
+};
 
 /**
  * Show only icons that match the search value.
  * @param {*} event
  */
 function searchicon(event) {
-    let modal = event.target.closest('.block_floatingbutton-iconpicker');
-    let search = event.target.closest('.block_floatingbutton-iconpicker-search-input');
-    let icons = Array.from(modal.querySelectorAll('.block_floatingbutton-iconpicker-icon'));
+    let modal = event.target.closest(SELECTORS.iconpicker);
+    let search = event.target.closest(SELECTORS.iconpickerSearchInput);
+    let icons = Array.from(modal.querySelectorAll(SELECTORS.iconpickerIcon));
     icons.forEach(function(icon) {
         if (
             icon.getAttribute('data-search').includes(search.value)
@@ -92,8 +104,8 @@ function searchicon(event) {
  * Adds class "highlight" to currently selected icon.
  */
 function highlightselected() {
-    let icons = Array.from(document.getElementsByClassName('block_floatingbutton-iconpicker-icon'));
-    let input = document.querySelector('.block_floatingbutton-iconpicker').getAttribute('data-iconpicker-input');
+    let icons = Array.from(document.querySelectorAll(SELECTORS.iconpickerIcon));
+    let input = document.querySelector(SELECTORS.iconpicker).getAttribute('data-iconpicker-input');
     if (!(input === null)) {
         let iconclass = document.querySelector(`[name="${input}"]`).getAttribute('value');
         icons.forEach(function(icondiv) {
@@ -110,11 +122,11 @@ function highlightselected() {
  * @param {*} event
  */
 function iconclick(event) {
-    let modal = event.target.closest('.block_floatingbutton-iconpicker');
+    let modal = event.target.closest(SELECTORS.iconpicker);
     let target = modal.getAttribute('data-iconpicker');
     let input = modal.getAttribute('data-iconpicker-input');
     if (target) {
-        let icondiv = event.target.closest('.block_floatingbutton-iconpicker-icon');
+        let icondiv = event.target.closest(SELECTORS.iconpickerIcon);
         let iconclass = document.querySelector('#' + icondiv.id + ' i').classList;
         document.getElementById(target).innerHTML = '<i class="' + iconclass + '"></i>';
         document.querySelector(`[name="${input}"]`).setAttribute('value', iconclass);
--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->release = '0.1.4';
-$plugin->version = 2025081601;
+$plugin->version = 2025102700;
 $plugin->requires = 2022041900;
 $plugin->component = 'block_floatingbutton';
 $plugin->supports = [401, 501];
